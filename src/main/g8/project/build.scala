13d432507f986acd12b42c1410b362478556f71c
Kazuhiro Sera <seratch@gmail.com>
2012-04-27 14:58:24 +0900

    Updated build.scala

--- src/main/g8/project/build.scala
+++ src/main/g8/project/build.scala
@@ -1,21 +1,13 @@
 import sbt._
 import Keys._
 
-object ScalikeJDBCBuild extends Build {
-
-  lazy val jdbcPattern = Project("scalikejdbc", file("."), settings = mainSettings)
+object $name$Build extends Build {
 
   lazy val mainSettings: Seq[Project.Setting[_]] = Defaults.defaultSettings ++ Seq(
     sbtPlugin := false,
-    organization := "com.github.seratch",
-    name := "scalikejdbc",
-    version := "0.6.4",
-    publishTo <<= version { (v: String) =>
-      val nexus = "https://oss.sonatype.org/"
-        if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots") 
-        else Some("releases"  at nexus + "service/local/staging/deploy/maven2")
-    },
-    publishMavenStyle := true,
+    organization := "$organization$",
+    name := "$name$",
+    version := "$version$",
     scalacOptions ++= Seq("-deprecation", "-unchecked")
   )
 
